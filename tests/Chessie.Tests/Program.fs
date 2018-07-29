2f9eddd6ed73ef6a2f8e7b26cd2fd915fe9e5ba9
vain0x <vainzerox@gmail.com>
2018-07-30 00:34:14 +0900

    Add polyfills for FsUnit (maybe unnecessary)

--- tests/Chessie.Tests/Program.fs
+++ tests/Chessie.Tests/Program.fs
@@ -1,17 +1,4 @@
 ﻿module Chessie.TestRunner
 
-open System
-open System.Reflection
-
-type Program = class end
-
 [<EntryPoint>]
-let main argv = 
-
-#if NETSTANDARD1_5
-    let run = typeof<Program>.GetTypeInfo().Assembly |> NUnitLite.AutoRun
-    run.Execute(argv, (new NUnit.Common.ExtendedTextWrapper(Console.Out)), Console.In)
-#else
-    let run = NUnitLite.AutoRun()
-    run.Execute(argv)
-#endif
+let main _ = 0
